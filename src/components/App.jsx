d9307b93abcf93e994824de1deaa2e680795de74
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2023-01-01 21:41:02 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,23 +6,52 @@ import { P } from './ContactList/ContactList.styled';
 import { Filter } from './Filter/Filter';
 import { Section } from './Section/Section';
 export class App extends Component {
-  state = {
-    contacts: [
+  static defaultProps = {
+    initialContacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+  };
+  state = {
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      });
+    } else {
+      this.setState({
+        contacts: this.props.initialContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = (name, number) => {
-   if( this.state.contacts.find(
-      option => option.name.toLowerCase() === name.toLowerCase()
-    ))
-      { alert(`${name} is already in contacts list`); return false}
-      else{ this.setState(prevState => ({
-          contacts: [...prevState.contacts, { id: nanoid(), name, number }],
-        }));  return true}
+    if (
+      this.state.contacts.find(
+        option => option.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts list`);
+      return false;
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+      }));
+      return true;
+    }
   };
   deleteContacts = id => {
     this.setState(prevState => {
